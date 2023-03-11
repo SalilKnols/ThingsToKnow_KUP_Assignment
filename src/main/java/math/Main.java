e91d31b2e9d3dea183f9fc15084f0d043d2f8959
SalilKnols <salil.verma@knoldus.com>
2023-03-11 22:47:42 +0530

    Calculator

---
+++ src/main/java/math/Main.java
@@ -0,0 +1,21 @@
+package math;
+
+import math.Calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 10, b = 5;
+
+        int sum = Calculator.add(a, b);
+        int difference = Calculator.subtract(a, b);
+        int product = Calculator.multiply(a, b);
+        int quotient = Calculator.divide(a, b);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
